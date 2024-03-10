013045aadadb852a575be35d77d0e36480592478
Maheep Kumar <maheep.kumar@tinymesh.in>
2024-03-10 18:55:28 +0530

    add send repeatedly functionality

--- src/components/CommunicationPanel.tsx
+++ src/components/CommunicationPanel.tsx
@@ -1,96 +1,140 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { invoke } from "@tauri-apps/api";
 
 function CommunicationPanel() {
   const [communicationInput, setCommunicationInput] = useState<string>("");
+  const [sendInterval, setSendInterval] = useState(1000);
+  const [intervalRunning, setIntervalRunning] = useState(false);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const handleSubmit = async () => {
     let sendBytesResult = await invoke("send_bytes", {
       input: communicationInput,
     });
     console.info(`Sending bytes. Result: ${sendBytesResult}`);
   };
 
+  const handleDecrementInterval = () => {
+    if (sendInterval > 1) {
+      setSendInterval(sendInterval - 1);
+    }
+  };
+
+  const handleIncrementInterval = () => {
+    setSendInterval(sendInterval + 1);
+  };
+
+  const handleRepeatToggle = () => {
+    if (intervalRunning) {
+      clearInterval(intervalId!);
+      setIntervalRunning(false);
+      setSendInterval(sendInterval); // Reset the interval to default value
+    } else {
+      const id = setInterval(async () => {
+        await handleSubmit();
+      }, sendInterval);
+      setIntervalId(id);
+      setIntervalRunning(true);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [intervalId]);
+
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-          <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
-            <label htmlFor="comment" className="sr-only">
-              Your message
-            </label>
-            <textarea
-              rows={4}
-              spellCheck={false}
-              className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-              placeholder="Write a message..."
-              onChange={(e) => setCommunicationInput(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+      <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+        <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+          <label htmlFor="comment" className="sr-only">
+            Your message
+          </label>
+          <textarea
+            rows={4}
+            spellCheck={false}
+            className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+            placeholder="Write a message..."
+            onChange={(e) => setCommunicationInput(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+          <span className="flex items-center space-x-2">
             <button
-              type="submit"
+              onClick={handleSubmit}
               className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
             >
-              Send Message
+              Send
             </button>
-            <div className="flex ps-0 space-x-1 rtl:space-x-reverse sm:ps-2">
+            <button
+              onClick={handleRepeatToggle}
+              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+            >
+              {intervalRunning ? "Cancel" : "Send Repeatedly"}
+            </button>
+          </span>
+
+          <div className="flex ps-0 space-x-1 rtl:space-x-reverse sm:ps-2">
+            <div className="relative flex items-center max-w-[10rem]">
               <button
                 type="button"
-                className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                onClick={handleDecrementInterval}
+                className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
               >
                 <svg
-                  className="w-4 h-4"
+                  className="w-3 h-3 text-gray-900 dark:text-white"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
-                  viewBox="0 0 12 20"
+                  viewBox="0 0 18 2"
                 >
                   <path
                     stroke="currentColor"
+                    stroke-linecap="round"
                     stroke-linejoin="round"
                     stroke-width="2"
-                    d="M1 6v8a5 5 0 1 0 10 0V4.5a3.5 3.5 0 1 0-7 0V13a2 2 0 0 0 4 0V6"
+                    d="M1 1h16"
                   />
                 </svg>
-                <span className="sr-only">Attach file</span>
               </button>
+              <input
+                type="text"
+                aria-describedby="Send interval in milliseconds"
+                className="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value={sendInterval}
+                disabled={intervalRunning}
+                onChange={(e) => setSendInterval(parseInt(e.target.value))}
+                required
+              />
               <button
                 type="button"
-                className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
+                onClick={handleIncrementInterval}
+                className="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none"
               >
                 <svg
-                  className="w-4 h-4"
+                  className="w-3 h-3 text-gray-900 dark:text-white"
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 16 20"
+                  fill="none"
+                  viewBox="0 0 18 18"
                 >
-                  <path d="M8 0a7.992 7.992 0 0 0-6.583 12.535 1 1 0 0 0 .12.183l.12.146c.112.145.227.285.326.4l5.245 6.374a1 1 0 0 0 1.545-.003l5.092-6.205c.206-.222.4-.455.578-.7l.127-.155a.934.934 0 0 0 .122-.192A8.001 8.001 0 0 0 8 0Zm0 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6Z" />
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M9 1v16M1 9h16"
+                  />
                 </svg>
-                <span className="sr-only">Set location</span>
-              </button>
-              <button
-                type="button"
-                className="inline-flex justify-center items-center p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600"
-              >
-                <svg
-                  className="w-4 h-4"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 18"
-                >
-                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
-                </svg>
-                <span className="sr-only">Upload image</span>
               </button>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </>
   );
 }
