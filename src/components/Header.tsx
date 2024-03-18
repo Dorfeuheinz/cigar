1e14eb9667f55d8bfbe6355570eea47859be28b6
Maheep Kumar Kathuria <me@maheepk.net>
2024-03-18 12:26:48 +0530

    Bring device out of config mode on disconnection

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import DeviceSelect from "./DeviceSelect";
 import InputWithDatalist from "./InputWithDatalist";
 import {
@@ -8,12 +8,14 @@ import {
 
 import ConnectDisconnectButton from "./ConnectDisconnectButton";
 import ConfigModeToggle from "./ConfigModeToggle";
+import { ConnectionContext } from "../App";
 import { invoke } from "@tauri-apps/api";
 import { message } from "@tauri-apps/api/dialog";
 
 function Header() {
   const [baudRate, setBaudRate] = useState<number>(19200);
   const [deviceName, setDeviceName] = useState("");
+  const { currentMode, setCurrentMode } = useContext(ConnectionContext);
 
   return (
     <header className="w-full justify-between bg-blue-500 p-2 text-white">
@@ -43,6 +45,8 @@ function Header() {
             connectFunction={async () => {
               try {
                 await connectToDevice(deviceName, baudRate);
+                await invoke("send_bytes", { input: "X" });
+                setCurrentMode("communication");
                 await invoke("start_communication_task", {});
                 return true;
               } catch (error) {
@@ -58,6 +62,8 @@ function Header() {
             }}
             disconnectFunction={async () => {
               await invoke("stop_communication_task", {});
+              await invoke("send_bytes", { input: "X" });
+              setCurrentMode("communication");
               return await disconnectFromDevice();
             }}
             className="inline-flex items-center rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
